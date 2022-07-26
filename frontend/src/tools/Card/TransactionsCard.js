336137415cffb1203ccb5bed089d276e99e0f2f7
Azmain951 <azmainafridi@gmail.com>
2022-07-26 13:22:54 +0600

    transaction page done with mock data

--- frontend/src/tools/Card/TransactionsCard.js
+++ frontend/src/tools/Card/TransactionsCard.js
@@ -7,7 +7,7 @@ const TransactionsCard = ({ data }) => {
                 <p className='w-1/6 text-center'>{data?.opportunity_name}</p>
                 <p className='w-1/6 text-center'>{data?.date}</p>
                 <p className='w-1/6 text-center'>{data?.type}</p>
-                <p className='w-1/6 text-center'>{data?.amount} {process.env.REACT_APP_TOKEN_NAME}</p>
+                <p className='w-1/6 text-center'>{data?.type === 'Drawdown' ? '+' : '-'}{data?.amount} {process.env.REACT_APP_TOKEN_NAME}</p>
                 {data?.status === 'Completed' && <p className='w-1/6 text-center'><button className='btn btn-xs btn-success'>Completed</button></p>}
                 {data?.status === 'Not Completed' && <p className='w-1/6 text-center'><button className='btn btn-xs btn-error'>Not Completed</button></p>}
                 {data?.status === 'Processing' && <p className='w-1/6 text-center'><button className='btn btn-xs btn-warning'>Processing</button></p>}
