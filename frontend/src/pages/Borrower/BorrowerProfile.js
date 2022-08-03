6a73224ad80cf6ee8426335e57ab8183a9f57cad
DnyaneshWarade <DnyaneshWarade@github.com>
2022-08-03 10:22:37 +0530

    Borrowers profile connection with web3 storage

--- frontend/src/pages/Borrower/BorrowerProfile.js
+++ frontend/src/pages/Borrower/BorrowerProfile.js
@@ -1,63 +1,224 @@
-import React from 'react';
-import DocumentCard from '../../tools/Card/DocumentCard';
-import { useNavigate } from 'react-router';
+import React, { useEffect, useState } from "react";
+import DocumentCard from "../../tools/Card/DocumentCard";
+import { useNavigate } from "react-router";
+import { retrieveFiles } from "../../services/web3storageIPFS";
+import {
+  getBinaryFileData,
+  getDataURLFromFile,
+} from "../../services/fileHelper";
 
 const BorrowerProfile = () => {
-    const navigate = useNavigate();
-    return (
-        <div>
-            <div className='mb-16 '>
-                <h2 className='mb-6 text-2xl font-medium'>Borrower Profile</h2>
-                <div style={{ display: 'flex' }} className='justify-between items-center mb-6'>
-                    <div style={{ display: 'flex' }}>
-                        <div class="avatar">
-                            <div class="w-16 rounded-full">
-                                <img src="https://placeimg.com/192/192/people" alt='' />
-                            </div>
-                        </div>
-                        <div style={{ display: 'flex' }} className='flex-col justify-center ml-4'>
-                            <h4 className='text-md'>Company Name</h4>
-                            <p className='text-xs'>Owner Name</p>
-                        </div>
-                    </div>
-                    <button onClick={() => navigate('/borrower_dashboard/edit_profile')} style={{ borderRadius: '100px', padding: '8px 16px' }} className='btn btn-sm btn-outline text-white'>Edit Profile</button>
-                </div>
-                <div style={{ display: 'flex' }} className='w-full justify-between mb-6'>
-                    <div className='w-1/2'>
-                        <h5 className='text-lg'>Socials</h5>
-                    </div>
-                    <div style={{ display: 'flex' }} className='w-1/2 justify-end'>
-                        <button style={{ borderRadius: '100px', padding: '8px 16px' }} className='ml-3 btn btn-sm btn-outline text-white'>Twitter</button>
-                        <button style={{ borderRadius: '100px', padding: '8px 16px' }} className='ml-3 btn btn-sm btn-outline text-white'>LinkedIn</button>
-                        <button style={{ borderRadius: '100px', padding: '8px 16px' }} className='ml-3 btn btn-sm btn-outline text-white'>Email</button>
-                        <button style={{ borderRadius: '100px', padding: '8px 16px' }} className='ml-3 btn btn-sm btn-outline text-white'>Website</button>
-                    </div>
-                </div>
-                <div className='mb-6'>
-                    <h5 className='text-lg'>Bio</h5>
-                    <p className='text-sm font-light text-justify'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus atque quisquam tenetur eos corporis reprehenderit facilis dolor vero soluta vitae, quo doloremque molestiae aperiam nostrum! Similique veniam eos ad dolor laborum, illum ullam quod numquam quaerat cum officiis accusantium placeat, eaque nostrum molestias, voluptatibus quidem! Dolores veniam tempora quos molestias amet similique maiores odit, eum nesciunt soluta fugit! Excepturi maiores sint, quas quisquam placeat eum maxime quasi qui praesentium eaque necessitatibus. Quidem sapiente inventore ea porro nisi, in hic perspiciatis amet nihil labore officia magnam, tempora deleniti corporis at totam quod praesentium vel? Velit officia quos qui, nulla, laborum quae recusandae incidunt aperiam et optio quo veritatis placeat? Accusantium minima ut delectus vitae saepe? Maiores quod veritatis beatae blanditiis laboriosam.</p>
-                </div>
+  const navigate = useNavigate();
 
-                <div className='mb-6'>
-                    <h5 className='text-lg'>KYC Details</h5>
-                    <DocumentCard></DocumentCard>
-                    <DocumentCard></DocumentCard>
-                </div>
-                <div className='mb-6'>
-                    <h5 className='text-lg'>KYB Details</h5>
-                    <h6 className='text-neutral text-sm'>Business Identify Proof</h6>
-                    <DocumentCard></DocumentCard>
-                    <DocumentCard></DocumentCard>
-                    <h6 className='text-neutral text-sm'>Business Address Proof</h6>
-                    <DocumentCard></DocumentCard>
-                    <h6 className='text-neutral text-sm'>Business License Proof</h6>
-                    <DocumentCard></DocumentCard>
-                    <h6 className='text-neutral text-sm'>Business Incorporation Proof</h6>
-                    <DocumentCard></DocumentCard>
-                </div>
+  const [borrowerJson, setborrowerJson] = useState();
+  const [logoImgSrc, setLogoImgSrc] = useState();
+  const [companyName, setCompanyName] = useState();
+  const [companyRepName, setCompanyRepName] = useState();
+  const [companyBio, setCompanyBio] = useState();
+  const [website, setWebsite] = useState();
+  const [email, setEmail] = useState();
+  const [twitter, setTwitter] = useState();
+  const [linkedin, setLinkedin] = useState();
+
+  useEffect(async () => {
+    // make the call to get borrower specific cid to fetch the data
+    // currently we'll mock the cid
+    let borrowerDataCID =
+      "bafybeiaqaqrrd7r7pir2j3rzsr35qi74y2cndjnqd2qadvmlzzzn7jtayu";
+    retrieveFiles(borrowerDataCID, true).then((res) => {
+      if (res) {
+        let read = getBinaryFileData(res);
+        read.onloadend = function () {
+          let brJson = JSON.parse(read.result);
+          loadBorrowerData(brJson);
+          setborrowerJson(brJson);
+        };
+      } else {
+        // load the empty profile page
+      }
+    });
+  }, [borrowerJson]);
+
+  const fetchBorrowerLogo = (imgcid) => {
+    if (imgcid) {
+      try {
+        retrieveFiles(imgcid, true).then((imgFile) => {
+          if (imgFile) {
+            let read = getDataURLFromFile(imgFile);
+            read.onloadend = function () {
+              setLogoImgSrc(read.result);
+            };
+          } else {
+            // set the empty logo image
+          }
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  const loadBorrowerProfileData = (profileData) => {
+    if (profileData) {
+      try {
+        if (profileData.companyName) {
+          setCompanyName(profileData.companyName);
+        }
+        if (profileData.companyRepName) {
+          setCompanyRepName(profileData.companyRepName);
+        }
+        if (profileData.companyBio) {
+          setCompanyBio(profileData.companyBio);
+        }
+        if (profileData.website) {
+          setWebsite(profileData.website);
+        }
+        if (profileData.email) {
+          setEmail(profileData.email);
+        }
+        if (profileData.twitter) {
+          setTwitter(profileData.twitter);
+        }
+        if (profileData.linkedin) {
+          setLinkedin(profileData.linkedin);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  const loadBorrowerData = (jsonData) => {
+    try {
+      if (jsonData) {
+        // Load the Logo image if there is any
+        fetchBorrowerLogo(jsonData.companyLogoCID);
+        // Load rest of the data
+        loadBorrowerProfileData(jsonData);
+
+        console.log("work in progress");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const redirectToURl = (event) => {
+    let url;
+    switch (event.target.id) {
+      case "twitter":
+        url = twitter;
+        break;
+      case "linkedin":
+        url = linkedin;
+        break;
+      case "website":
+        url = website;
+        break;
+    }
+
+    if (url) {
+      window.open(url, "_blank");
+    }
+  };
+
+  return (
+    <div>
+      <div className="mb-16 ">
+        <h2 className="mb-6 text-2xl font-medium">Borrower Profile</h2>
+        <div
+          style={{ display: "flex" }}
+          className="justify-between items-center mb-6"
+        >
+          <div style={{ display: "flex" }}>
+            <div class="avatar">
+              <div class="w-16 rounded-full">
+                <img src={logoImgSrc} />
+              </div>
             </div>
+            <div
+              style={{ display: "flex" }}
+              className="flex-col justify-center ml-4"
+            >
+              <h4 className="text-md">{companyName}</h4>
+              <p className="text-xs">{companyRepName}</p>
+            </div>
+          </div>
+          <button
+            onClick={() =>
+              navigate("/borrower_dashboard/edit_profile", borrowerJson)
+            }
+            style={{ borderRadius: "100px", padding: "8px 16px" }}
+            className="btn btn-sm btn-outline text-white"
+          >
+            Edit Profile
+          </button>
         </div>
-    );
+        <div
+          style={{ display: "flex" }}
+          className="w-full justify-between mb-6"
+        >
+          <div className="w-1/2">
+            <h5 className="text-lg">Socials</h5>
+          </div>
+          <div style={{ display: "flex" }} className="w-1/2 justify-end">
+            <button
+              id="twitter"
+              style={{ borderRadius: "100px", padding: "8px 16px" }}
+              className="ml-3 btn btn-sm btn-outline text-white"
+              onClick={redirectToURl}
+            >
+              Twitter
+            </button>
+            <button
+              id="linkedin"
+              style={{ borderRadius: "100px", padding: "8px 16px" }}
+              className="ml-3 btn btn-sm btn-outline text-white"
+              onClick={redirectToURl}
+            >
+              LinkedIn
+            </button>
+            <button
+              style={{ borderRadius: "100px", padding: "8px 16px" }}
+              className="ml-3 btn btn-sm btn-outline text-white"
+            >
+              Email
+            </button>
+            <button
+              id="website"
+              style={{ borderRadius: "100px", padding: "8px 16px" }}
+              className="ml-3 btn btn-sm btn-outline text-white"
+              onClick={redirectToURl}
+            >
+              Website
+            </button>
+          </div>
+        </div>
+        <div className="mb-6">
+          <h5 className="text-lg">Bio</h5>
+          <p className="text-sm font-light text-justify"> {companyBio} </p>
+        </div>
+
+        <div className="mb-6">
+          <h5 className="text-lg">KYC Details</h5>
+          <DocumentCard></DocumentCard>
+          <DocumentCard></DocumentCard>
+        </div>
+        <div className="mb-6">
+          <h5 className="text-lg">KYB Details</h5>
+          <h6 className="text-neutral text-sm">Business Identify Proof</h6>
+          <DocumentCard></DocumentCard>
+          <DocumentCard></DocumentCard>
+          <h6 className="text-neutral text-sm">Business Address Proof</h6>
+          <DocumentCard></DocumentCard>
+          <h6 className="text-neutral text-sm">Business License Proof</h6>
+          <DocumentCard></DocumentCard>
+          <h6 className="text-neutral text-sm">Business Incorporation Proof</h6>
+          <DocumentCard></DocumentCard>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BorrowerProfile;
