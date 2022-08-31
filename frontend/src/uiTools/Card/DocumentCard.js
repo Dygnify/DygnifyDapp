80bcec8e77b187be72758260430f8cb2a8ff505d
Your Name <you@example.com>
2022-08-31 23:29:09 +0530

    Borrow Form updates

--- frontend/src/uiTools/Card/DocumentCard.js
+++ frontend/src/uiTools/Card/DocumentCard.js
@@ -2,39 +2,25 @@ import React, { useEffect, useState } from "react";
 import { getIPFSFileURL } from "../../services/web3storageIPFS";
 
 const DocumentCard = ({ docName, docCid, fileName, disable }) => {
-  const viewDoc = () => {
-    if (!docCid) return null;
-    let url = getIPFSFileURL(docCid);
-    if (fileName) url += `/${fileName}`;
-    console.log(fileName);
-    window.open(url, "_blank");
-  };
+	const viewDoc = () => {
+		if (!docCid) return null;
+		let url = getIPFSFileURL(docCid);
+		if (fileName) url += `/${fileName}`;
+		console.log(fileName);
+		window.open(url, "_blank");
+	};
 
-  return (
-    <div
-      style={{
-        backgroundColor: "#20232A",
-        borderRadius: "8px",
-        display: "flex",
-        padding: "11px 16px",
-      }}
-      className="justify-between mb-2"
-    >
-      <div>
-        <p>{docName ? null : "view document"}</p>
-        {docName ? (
-          <p style={{ fontStyle: "italic", color: "#E6E6E6" }}> {docName}</p>
-        ) : null}
-      </div>
-      <a
-        className="text-blue-700"
-        style={{ cursor: "pointer" }}
-        onClick={viewDoc}
-      >
-        {disable ? null : "view document"}
-      </a>
-    </div>
-  );
+	return (
+		<div className="justify-between mb-2 bg-[#20232A] rounded-lg flex px-4 py-3">
+			<div>
+				<p>{docName ? null : "view document"}</p>
+				{docName ? <p className="italic bg-[#E6E6E6]"> {docName}</p> : null}
+			</div>
+			<a className="text-blue-700 cursor-pointer" onClick={viewDoc}>
+				{disable ? null : "view document"}
+			</a>
+		</div>
+	);
 };
 
 export default DocumentCard;
