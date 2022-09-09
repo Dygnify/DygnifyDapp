b3e5164e2f05c5d8290668f17184c831785cb08b
rohan <80212018+rohan031@users.noreply.github.com>
2022-09-09 19:01:11 +0530

    investor transaction theme and responsiveness

--- frontend/src/pages/Investor/Transactions.js
+++ frontend/src/pages/Investor/Transactions.js
@@ -22,31 +22,28 @@ const Transactions = () => {
 	}, []);
 
 	return (
-		<div className={`relative ${loading ? "h-[100vh]" : ""}`}>
+		<div className={`relative mb-16 ${loading ? "h-[100vh]" : ""}`}>
 			{loading && <Loader />}
-			<div className={`mb-16 ${loading ? "blur-sm" : ""}`}>
-				<h2 className="text-2xl mb-6">Transaction History</h2>
+			<div className={` ${loading ? "filter blur-sm" : ""}`}>
+				<h2 className="font-semibold text-[1.4375rem] lg:text-[2.0625rem]">
+					Transaction History
+				</h2>
 				{transactions?.length !== 0 ? (
 					<>
-						<div className="collapse mb-3">
-							<input type="checkbox" className="peer" />
-							<div
-								style={{
-									display: "flex",
-									borderTop: "1px solid #20232A",
-									borderBottom: "1px solid #20232A",
-								}}
-								className="collapse-title text-md font-normal justify-around w-full"
-							>
-								<p className="w-1/6 text-center">Pool</p>
-								<p className="w-1/6 text-center">Date</p>
-								<p className="w-1/6 text-center">Transaction Type</p>
-								<p className="w-1/6 text-center">Amount</p>
-								<p className="w-1/6 text-center">Status</p>
-								<p className="w-1/6 text-center">View on Explorer</p>
-							</div>
+						<div className="px-1 mt-8 py-6 gap-4 md:gap-0 md:justify-around flex font-bold border-y border-neutral-300 dark:border-darkmode-500 text-center">
+							<p className="w-1/3 md:w-1/6 my-auto ">Pool</p>
+							<p className="hidden md:block w-1/3 md:w-1/6 my-auto ">Date</p>
+							<p className="hidden md:block w-1/3 md:w-1/6 my-auto ">
+								Transaction Type
+							</p>
+							<p className=" w-1/3 md:w-1/6 my-auto ">Amount</p>
+							<p className="w-1/3 md:w-1/6 my-auto ">Status</p>
+							<p className="hidden md:block w-1/3 md:w-1/6 my-auto ">
+								View on Explorer
+							</p>
 						</div>
-						<div>
+
+						<div className="my-5 flex flex-col gap-3">
 							{transactions
 								? transactions.map((item) => (
 										<TransactionsCard key={transactions.hash} data={item} />
@@ -55,10 +52,8 @@ const Transactions = () => {
 						</div>
 					</>
 				) : (
-					<div style={{ display: "flex" }} className="justify-center">
-						<div style={{ color: "#64748B", fontSize: 18 }}>
-							No transactions available.
-						</div>
+					<div className="flex justify-center">
+						<div className="text-neutral-500">No transactions available.</div>
 					</div>
 				)}
 			</div>
