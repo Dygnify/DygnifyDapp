d76fc4af55911e364df707accdc5dea3198e8460
rohan <80212018+rohan031@users.noreply.github.com>
2022-09-10 19:58:42 +0530

    landing gradient

--- frontend/src/pages/LandingPage/LandingPage.js
+++ frontend/src/pages/LandingPage/LandingPage.js
@@ -14,56 +14,58 @@ const LandingPage = () => {
 
 	return (
 		<div className={`${darkMode ? "dark" : ""} `}>
-			<div className="dark:bg-[#000000] w-full bg-white dark:text-white text-black pb-20 md:pb-32 xl:pb-20">
-				<Header darkMode={darkMode} setDarkMode={setDarkMode} />
+			<div className="dark:bg-[#000000] w-full bg-white dark:text-white text-black pb-20 md:pb-32 xl:pb-20 h-[100vh]">
 				<div className="landing-backgroundimage">
-					<div className="flex-col items-center flex pt-20 xl:pt-10">
-						<h1 className="text-4xl font-bold text-center w-[90%] px-auto md:w-[78%] md:text-5xl xl:text-[3.1rem] xl:w-[50%] 2xl:text-[3.5]">
-							Democratising Credit Flow to Small Business
-						</h1>
+					<Header darkMode={darkMode} setDarkMode={setDarkMode} />
+					<div>
+						<div className="flex-col items-center flex pt-20 xl:pt-10">
+							<h1 className="text-4xl font-bold text-center w-[90%] px-auto md:w-[78%] md:text-5xl xl:text-[3.1rem] xl:w-[50%] 2xl:text-[3.5]">
+								Democratising Credit Flow to Small Business
+							</h1>
 
-						<h6 className="font-medium text-base w-[80%] text-center mx-auto my-6 md:w-[55%] xl:w-[33%] 2xl:w-[30%]">
-							DeFi platform enabling stable, attractive yields for digital asset
-							investors via purpose-based investments in emerging market
-							opportunities
-						</h6>
-					</div>
-					<br />
-
-					<div className="flex flex-col w-full items-center gap-24 md:gap-0 md:flex-row md:px-5 md:justify-evenly lg:px-32 xl:px-60 2xl:px-64">
-						<div className="flex flex-col justify-center items-center">
-							<img
-								src={darkMode ? InvestImage : InvestImageLightmode}
-								className="w-72 px-auto xl:w-[18rem] 2xl:w-[21rem]"
-							/>
-							<h6 className="text-center py-3 w-[80%] font-medium text-base">
-								Invest USDC in real world assets and earn attractive risk
-								adjusted returns.
+							<h6 className="font-medium text-base w-[80%] text-center mx-auto my-6 md:w-[55%] xl:w-[33%] 2xl:w-[30%]">
+								DeFi platform enabling stable, attractive yields for digital
+								asset investors via purpose-based investments in emerging market
+								opportunities
 							</h6>
-							<GradientButton
-								onClick={() => {
-									path("/investor-dashboard/overview");
-								}}
-							>
-								Invest
-							</GradientButton>
 						</div>
-						<div className="flex flex-col justify-center items-center">
-							<img
-								src={darkMode ? BorrowImage : BorrowImageLightmode}
-								className="w-72 px-auto xl:w-[18rem] 2xl:w-[21rem]"
-							/>
-							<h6 className="text-center py-3 w-[80%] font-medium text-base">
-								On tap liquidity in a transaparent and hassle free manner at
-								competitive prices.
-							</h6>
-							<GradientButton
-								onClick={() => {
-									path("/borrower_dashboard/overview");
-								}}
-							>
-								Borrow
-							</GradientButton>
+						<br />
+
+						<div className="flex flex-col w-full items-center gap-24 md:gap-0 md:flex-row md:px-5 md:justify-evenly lg:px-32 xl:px-60 2xl:px-64">
+							<div className="flex flex-col justify-center items-center">
+								<img
+									src={darkMode ? InvestImage : InvestImageLightmode}
+									className="w-72 px-auto xl:w-[18rem] 2xl:w-[21rem]"
+								/>
+								<h6 className="text-center py-3 w-[80%] font-medium text-base">
+									Invest USDC in real world assets and earn attractive risk
+									adjusted returns.
+								</h6>
+								<GradientButton
+									onClick={() => {
+										path("/investor-dashboard/overview");
+									}}
+								>
+									Invest
+								</GradientButton>
+							</div>
+							<div className="flex flex-col justify-center items-center">
+								<img
+									src={darkMode ? BorrowImage : BorrowImageLightmode}
+									className="w-72 px-auto xl:w-[18rem] 2xl:w-[21rem]"
+								/>
+								<h6 className="text-center py-3 w-[80%] font-medium text-base">
+									On tap liquidity in a transaparent and hassle free manner at
+									competitive prices.
+								</h6>
+								<GradientButton
+									onClick={() => {
+										path("/borrower_dashboard/overview");
+									}}
+								>
+									Borrow
+								</GradientButton>
+							</div>
 						</div>
 					</div>
 				</div>
