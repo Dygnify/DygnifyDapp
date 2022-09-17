e64719cc4994c0034a110c8a55deb78afd3025cb
Your Name <you@example.com>
2022-09-17 23:06:25 +0530

    commonConnectors error handling

--- frontend/src/services/BackendConnectors/userConnectors/commonConnectors.js
+++ frontend/src/services/BackendConnectors/userConnectors/commonConnectors.js
@@ -22,28 +22,38 @@ const getEthAddress = async () => {
 };
 
 const requestAccount = async (metaMask) => {
-	if (typeof window.ethereum !== "undefined") {
-		let provider = window.ethereum;
-		// edge case if MM and CBW are both installed
-		if (window.ethereum.providers?.length) {
-			window.ethereum.providers.forEach(async (p) => {
-				if (metaMask === true) {
-					if (p.isMetaMask) provider = p;
-				} else {
-					if (p.isCoinbaseWallet) {
-						provider = p;
+	try {
+		if (typeof window.ethereum !== "undefined") {
+			let provider = window.ethereum;
+			// edge case if MM and CBW are both installed
+			if (window.ethereum.providers?.length) {
+				window.ethereum.providers.forEach(async (p) => {
+					if (metaMask === true) {
+						if (p.isMetaMask) provider = p;
+					} else {
+						if (p.isCoinbaseWallet) {
+							provider = p;
+						}
 					}
-				}
+				});
+			}
+			await provider.request({
+				method: "wallet_switchEthereumChain",
+				params: [{ chainId: "0x13881" }], // chainId must be in hexadecimal numbers
 			});
+			await provider.request({
+				method: "eth_requestAccounts",
+				params: [],
+			});
+
+			return { success: true };
 		}
-		await provider.request({
-			method: "wallet_switchEthereumChain",
-			params: [{ chainId: "0x13881" }], // chainId must be in hexadecimal numbers
-		});
-		await provider.request({
-			method: "eth_requestAccounts",
-			params: [],
-		});
+	} catch (error) {
+		Sentry.captureException(error);
+		return {
+			success: false,
+			msg: error.message,
+		};
 	}
 };
 
