77868c5444d17350ce8a94078a48161d0016984e
Gautam <bansalgautam@gmail.com>
2022-10-05 18:40:02 +0530

    adding MultiSign to Dygnify Treasury

--- smartcontracts/contracts/protocol/DygnifyTreasury.sol
+++ smartcontracts/contracts/protocol/DygnifyTreasury.sol
@@ -5,12 +5,18 @@ import "./DygnifyConfig.sol";
 import "./BaseUpgradeablePausable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "./MultiSign.sol";
 
 contract DygnifyTreasury is BaseUpgradeablePausable {
     DygnifyConfig public dygnifyConfig;
     using ConfigHelper for DygnifyConfig;
     IERC20 public usdcToken;
     using SafeERC20 for IERC20;
+    address wallet;
+
+    function setWallet(address _wallet) onlyAdmin public{
+        wallet = _wallet;
+    }
 
     function initialize(DygnifyConfig _dygnifyConfig) external initializer {
         require(
@@ -30,12 +36,27 @@ contract DygnifyTreasury is BaseUpgradeablePausable {
         return balance; 
     }
 
-    function withdraw(uint256 amount)public nonReentrant whenNotPaused onlyAdmin{
+    // function withdraw(uint256 amount)public nonReentrant whenNotPaused onlyAdmin{
+    //     require(amount > 0, "amount must be greater than zero");
+    //     uint256 totalBalance = getTreasuryBalance();
+    //     require(amount <= totalBalance, "amount exceeds the total treasury balance");
+
+    //     usdcToken.safeTransferFrom(address(this), msg.sender, amount);
+    // }
+
+
+    function reqTx(address to,uint256 value)public{
+        MultiSigWallet obj = MultiSigWallet(payable(wallet));
+        obj.submitTransaction(to,value);
+    }
+
+
+    function withdraw(address to , uint256 amount)public nonReentrant whenNotPaused{
         require(amount > 0, "amount must be greater than zero");
+        require(msg.sender == wallet,"not allowed outside tranction");
         uint256 totalBalance = getTreasuryBalance();
         require(amount <= totalBalance, "amount exceeds the total treasury balance");
-
-        usdcToken.safeTransferFrom(address(this), msg.sender, amount);
+        usdcToken.safeTransferFrom(address(this), to, amount);
     }
 
 }
