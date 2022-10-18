b664e5dd142afdbeda8d5da60fb8f8f99c7f5d45
Dnyanesh Warade <dnyanesh@dygnify.com>
2022-10-18 21:42:23 +0530

    corrected the sorting logic in overview page

--- frontend/src/pages/Borrower/Overview.js
+++ frontend/src/pages/Borrower/Overview.js
@@ -99,8 +99,8 @@ const Overview = () => {
 
 	function sortByProperty(property) {
 		return function (a, b) {
-			if (a[property] < b[property]) return 1;
-			else if (a[property] > b[property]) return -1;
+			if (a[property] < b[property]) return -1;
+			else if (a[property] > b[property]) return 1;
 
 			return 0;
 		};
