89cb7548e1d88675aa04ec1f9ee126c37db7325e
rohan <80212018+rohan031@users.noreply.github.com>
2022-11-05 23:17:06 +0530

    progress uploader

--- frontend/src/services/Helpers/web3storageIPFS.js
+++ frontend/src/services/Helpers/web3storageIPFS.js
@@ -1,16 +1,47 @@
 import { Web3Storage, File } from "web3.storage";
 import { Buffer } from "buffer";
 const Sentry = require("@sentry/react");
+let fileName = "file";
+let uploaded = 0;
+let totalSize;
+
+function progressEvent(progress, file, status) {
+	const event = new CustomEvent("progressDetail", {
+		detail: {
+			progress: progress,
+			file: file,
+			status: status,
+		},
+	});
+
+	document.dispatchEvent(event);
+}
+
 function makeStorageClient() {
 	return new Web3Storage({ token: process.env.REACT_APP_WEB3STORAGE_APIKEY });
 }
 
+const onStoredChunk = (size) => {
+	uploaded += size;
+	const pct = 100 * (uploaded / totalSize);
+
+	console.log(`Uploading... ${pct.toFixed(2)}% complete`);
+
+	if (fileName !== "savingOtherDataToWEB3Db")
+		progressEvent(pct.toFixed(2), fileName, "Uploading");
+};
+
 export async function storeFiles(files) {
 	Sentry.captureMessage("storeFiles", "info");
 	try {
+		uploaded = 0;
+		totalSize = files[0].size;
+		fileName = files[0].name ? files[0].name : "savingOtherDataToWEB3Db";
 		const client = makeStorageClient();
-		const cid = await client.put(files);
+		const cid = await client.put(files, { onStoredChunk });
 		console.log("stored files with cid:", cid);
+		if (fileName !== "savingOtherDataToWEB3Db")
+			progressEvent(100, fileName, "Uploaded");
 		return cid;
 	} catch (error) {
 		Sentry.captureException(error);
